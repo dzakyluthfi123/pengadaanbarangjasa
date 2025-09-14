7c53d31b535167347e0ab1d642e50f07c5643845
khafidzdev <khafidzrizziq@gmail.com>
2025-09-14 20:23:38 +0700

    30% admin dashboard

--- src/admin/index.php
+++ src/admin/index.php
@@ -134,8 +134,8 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">1</div>
-                                    <div class="card-body font-extrabold text-9xl">Proyek Selesai</div>
+                                    <div class="card-body"><b>1</b></div>
+                                    <div class="card-body font-extrabold text-9xl"><b>Proyek Selesai</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -145,7 +145,8 @@
 
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                                <div class="card-body"><b>1</b></div>
+                                    <div class="card-body"><b>Proyek Setengah Jalan</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -155,7 +156,8 @@
 
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
+                                <div class="card-body"><b>1</b></div>
+                                    <div class="card-body"><b>Proyek Selesai</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -165,7 +167,8 @@
 
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-body"><b>1</b></div>
+                                    <div class="card-body"><b>Keluhan Masyarakat Tentang Proyek</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -174,6 +177,7 @@
                             </div>
 
                         </div>
+                        <!-- Area Chart Example -->
                         <div class="row">
                             <div class="col-xl-6">
                                 <div class="card mb-4">
@@ -184,6 +188,7 @@
                                     <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
                                 </div>
                             </div>
+                            <!-- Bar Chart Example -->
                             <div class="col-xl-6">
                                 <div class="card mb-4">
                                     <div class="card-header">
@@ -194,6 +199,7 @@
                                 </div>
                             </div>
                         </div>
+                        <!-- DataTable Example -->
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
@@ -684,6 +690,7 @@
                         </div>
                     </div>
                 </main>
+                <!-- DataExample -->
                 <!-- sampai sini isinya -->
 
                 <!-- bagian footer -->
